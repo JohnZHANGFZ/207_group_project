bde62258c9d33ace5e39af5c0adb1a80d2edd2ad
JohnZHANGFZ <fangzheng.zhang@mail.utoronto.ca>
2023-11-10 17:11:18 -0500

    finish the implementation for CommonIngredient, CommonREcipe, CommonRestriction, CommonUser

--- src/entity/CommonIngredient.java
+++ src/entity/CommonIngredient.java
@@ -1,4 +1,14 @@
 package entity;
 
-public class CommonIngredient {
+public class CommonIngredient implements Ingredient{
+    private final String ingredient;
+
+    public CommonIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    @Override
+    public String getName() {
+        return this.ingredient;
+    }
 }
