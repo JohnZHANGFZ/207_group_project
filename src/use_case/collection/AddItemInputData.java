f661a591cd79cc30e2db1a3bac407a57590afe99
Alexa <alexahs.zhang@mail.utoronto.ca>
2023-11-11 16:12:49 -0500

    create usecase file with updated names for addItem

--- src/use_case/collection/AddItemInputData.java
+++ src/use_case/collection/AddItemInputData.java
@@ -1,4 +1,4 @@
 package use_case.collection;
 
-public class addItemInputData {
+public class AddItemInputData {
 }
--- src/use_case/collection/addItemInputBoundary.java
+++
@@ -1,4 +0,0 @@
-package use_case.collection;
-
-public interface addItemInputBoundary {
-}
