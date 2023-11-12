34c7ca110a3198cfb92d3b307bacd2023c5b6ca9
Alexa <alexahs.zhang@mail.utoronto.ca>
2023-11-12 01:01:49 -0500

    finish AddItemState, have AddItemPresenter implementing AddItemOutputBoundary, have AddItemViewModel extending ViewModel

--- src/interface_adapter/collection/add_item/AddItemPresenter.java
+++ src/interface_adapter/collection/add_item/AddItemPresenter.java
@@ -1,4 +1,22 @@
 package interface_adapter.collection.add_item;
 
-public class AddItemPresenter {
+import use_case.collection.AddItem.AddItemOutputBoundary;
+
+public class AddItemPresenter implements AddItemOutputBoundary {
+
+    private final AddItemViewModel addItemViewModel;
+
+    public AddItemPresenter(AddItemViewModel addItemViewModel) {
+        this.addItemViewModel = addItemViewModel;
+    }
+
+    @Override
+    public void prepareSuccessView() {
+
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+
+    }
 }
--- src/interface_adapter/collection/add_item/AddItemViewModel.java
+++ src/interface_adapter/collection/add_item/AddItemViewModel.java
@@ -1,4 +1,22 @@
 package interface_adapter.collection.add_item;
 
-public class AddItemViewModel {
+import interface_adapter.ViewModel;
+
+import javax.swing.text.View;
+import java.beans.PropertyChangeListener;
+
+public class AddItemViewModel extends ViewModel {
+    public AddItemViewModel(String viewName) {
+        super(viewName);
+    }
+
+    @Override
+    public void firePropertyChanged() {
+
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+
+    }
 }
