011712fb522b90e689edf017d0612c8bb4c9a014
Alexa <alexahs.zhang@mail.utoronto.ca>
2023-11-12 00:46:28 -0500

    update AddItemController

--- src/interface_adapter/collection/add_item/AddItemController.java
+++ src/interface_adapter/collection/add_item/AddItemController.java
@@ -1,4 +1,20 @@
 package interface_adapter.collection.add_item;
 
+import use_case.collection.AddItem.AddItemInputBoundary;
+import use_case.collection.AddItem.AddItemInputData;
+
+import java.util.List;
+
 public class AddItemController {
+    final AddItemInputBoundary addItemUseCaseInteractor;
+
+    public AddItemController(AddItemInputBoundary addItemUseCaseInteractor) {
+        this.addItemUseCaseInteractor = addItemUseCaseInteractor;
+    }
+
+    void execute(List<String> ingredients) {
+        AddItemInputData addItemInputData = new AddItemInputData(ingredients);
+
+        addItemUseCaseInteractor.execute(addItemInputData);
+    }
 }
