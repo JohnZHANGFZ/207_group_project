27afe4f079280a2ba30eabe9e3ceca78613f40a2
Alexa <alexahs.zhang@mail.utoronto.ca>
2023-11-12 01:07:43 -0500

    add methods for AddItemViewModel

--- src/interface_adapter/collection/add_item/AddItemViewModel.java
+++ src/interface_adapter/collection/add_item/AddItemViewModel.java
@@ -4,19 +4,29 @@ import interface_adapter.ViewModel;
 
 import javax.swing.text.View;
 import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 
 public class AddItemViewModel extends ViewModel {
+
+    private AddItemState state = new AddItemState();
+
     public AddItemViewModel(String viewName) {
         super(viewName);
     }
 
+    public void setState(AddItemState state) { this.state = state;}
+
+    public AddItemState getState() { return state; }
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
     @Override
     public void firePropertyChanged() {
-
+        support.firePropertyChange("state", null, this.state);
     }
 
     @Override
     public void addPropertyChangeListener(PropertyChangeListener listener) {
-
+        support.addPropertyChangeListener(listener);
     }
 }
