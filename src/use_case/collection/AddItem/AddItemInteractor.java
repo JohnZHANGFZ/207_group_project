f7e9969b4425b0f4cb2a2fe2cca6d9195d4b84d2
Alexa <alexahs.zhang@mail.utoronto.ca>
2023-11-12 01:20:16 -0500

    update execute for AddItemInteractor

--- src/use_case/collection/AddItem/AddItemInteractor.java
+++ src/use_case/collection/AddItem/AddItemInteractor.java
@@ -1,11 +1,39 @@
 package use_case.collection.AddItem;
 
-import use_case.collection.AddItem.AddItemInputBoundary;
-import use_case.collection.AddItem.AddItemInputData;
+import entity.Ingredient;
+import entity.IngredientFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddItemInteractor implements AddItemInputBoundary {
+
+    final AddItemDataAccessInterface addItemDataAccessObject;
+
+    final AddItemOutputBoundary addItemPresenter;
+
+    final IngredientFactory ingredientFactory;
+
+    public AddItemInteractor(AddItemDataAccessInterface addItemDataAccessObject,
+                             AddItemOutputBoundary addItemPresenter,
+                             IngredientFactory ingredientFactory) {
+        this.addItemDataAccessObject = addItemDataAccessObject;
+        this.addItemPresenter = addItemPresenter;
+        this.ingredientFactory = ingredientFactory;
+    }
+
     @Override
     public void execute(AddItemInputData addItemInputData) {
+        if(addItemInputData.getItems().isEmpty()) {
+            addItemPresenter.prepareFailView("No input.");
+        } else {
+            for(int i = 0; i < addItemInputData.getItems().size();) {
 
+                String item = addItemInputData.getItems().get(i);
+                Ingredient ingredient = ingredientFactory.create(item);
+                addItemDataAccessObject.save(ingredient);
+            }
+            addItemPresenter.prepareSuccessView();
+        }
     }
 }
