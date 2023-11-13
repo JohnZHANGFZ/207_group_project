771ef66e2cff6ae3f145fd96f7c3a59442723cff
Mike <wujimikechen@gmail.com>
2023-11-13 13:01:01 -0500

    made CreateDatabase more testable and added a test file and folder for CreateDatabase that has 100% coverage

--- src/database_creation/CreateDatabase.java
+++ src/database_creation/CreateDatabase.java
@@ -7,6 +7,10 @@ import com.opencsv.CSVWriter;
 import java.io.*;
 import java.util.ArrayList;
 
+/*
+This code solely exists to create the database and will never be run in the program
+ */
+
 public class CreateDatabase {
     final ArrayList<String> ingredients;
     final int number;
@@ -19,10 +23,10 @@ public class CreateDatabase {
     /*
     Adds more recipes to the database
      */
-    public void addToCsv() throws Exception {
+    public void addToCsv(String filepath) throws Exception {
         JsonArray recipes = QueryAPI.getResults(this.ingredients, this.number);
-        String filePath = "src/data_access/resources/database.csv";
-        File file = new File(filePath);
+        //String filePath = "src/data_access/resources/database.csv";
+        File file = new File(filepath);
 
         boolean append = file.length() > 0; //checks to see if file is empty
 
@@ -38,45 +42,43 @@ public class CreateDatabase {
             writer.writeNext(header);
         }
 
-        ArrayList<Integer> records = loadId(filePath);
-        try {
-            // add data to csv
-            for (int i = 0; i < recipes.size(); i++) {
-                JsonObject recipe = recipes.get(i).getAsJsonObject();
-                String id = recipe.get("id").getAsString();
-                String title = recipe.get("title").getAsString();
-                String image = recipe.get("image").getAsString();
-                String ingredients = String.valueOf(getIngredients(recipe));
-                String[] data = {id, title, image , String.valueOf(ingredients)};
-                if (!records.contains(Integer.valueOf(id))) {
-                    writer.writeNext(data);
-                }
+        ArrayList<Integer> records = loadId(filepath);
+        // add data to csv
+        for (int i = 0; i < recipes.size(); i++) {
+            JsonObject recipe = recipes.get(i).getAsJsonObject();
+            String id = recipe.get("id").getAsString();
+            String title = recipe.get("title").getAsString();
+            String image = recipe.get("image").getAsString();
+            String ingredients = String.valueOf(getIngredients(recipe));
+            String[] data = {id, title, image , String.valueOf(ingredients)};
+            if (!records.contains(Integer.valueOf(id))) {
+                writer.writeNext(data);
             }
-            // closing writer connection
-            writer.close();
         }
-        catch (IOException e) {
-            System.out.println("Input/Output exception thrown");
-            //e.printStackTrace();
-        }
-
+        // closing writer connection
+        writer.close();
     }
 
     /*
     Returns an arraylist of ID's of recipes inside the .csv
      */
-    public ArrayList<Integer> loadId(String fileName) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(fileName));
-        String line = br.readLine();
-        String DELIMITER = ",";
-        ArrayList<Integer> records = new ArrayList<>();
-        while ((line = br.readLine()) != null) {
-            String[] values = line.split(DELIMITER);
-            records.add(Integer.valueOf(values[0].replace("\"", "")));
+    public static ArrayList<Integer> loadId(String fileName) throws IOException {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            String line = br.readLine();
+            String DELIMITER = ",";
+            ArrayList<Integer> records = new ArrayList<>();
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(DELIMITER);
+                records.add(Integer.valueOf(values[0].replace("\"", "")));
+            }
+            return records;
         }
-        return records;
-    }
+        catch (IOException e){
+            throw new IOException();
+        }
 
+    }
     /*
     Converts a json recipe object into a list of ingredients so that we can store it in the database
      */
@@ -95,9 +97,10 @@ public class CreateDatabase {
 
     public static void main(String[] args) throws Exception {
         ArrayList<String> ingredients = new ArrayList<>();
-        ingredients.add("ginger");
-        int number = 200;
+        ingredients.add("carrot");
+        int number = 2;
         CreateDatabase database = new CreateDatabase(ingredients, number);
-        database.addToCsv();
+        String filepath = "src/data_access/resources/database.csv";
+        database.addToCsv(filepath);
     }
 }
