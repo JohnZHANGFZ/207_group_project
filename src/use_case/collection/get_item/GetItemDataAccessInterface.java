094945bf99d3b9c7273df8ee154d8dd3d5ee7f98
Alexa <alexahs.zhang@mail.utoronto.ca>
2023-11-19 11:19:46 -0500

    updated GetItemOutputBoundary and Data

--- src/use_case/collection/get_item/GetItemDataAccessInterface.java
+++ src/use_case/collection/get_item/GetItemDataAccessInterface.java
@@ -1,4 +1,9 @@
 package use_case.collection.get_item;
 
+import java.util.List;
+
 public interface GetItemDataAccessInterface {
+
+    boolean existByName(String itemName);
+    List<String> getItem();
 }
--- src/use_case/collection/get_item/GetItemOutputBoundary.java
+++ src/use_case/collection/get_item/GetItemOutputBoundary.java
@@ -1,4 +1,7 @@
 package use_case.collection.get_item;
 
 public interface GetItemOutputBoundary {
+    void prepareSuccessView();
+
+    void prepareFailView(String error);
 }
