952ea075778c38b41301a1d66b728cd975181cdf
Alexa <alexahs.zhang@mail.utoronto.ca>
2023-11-29 16:11:23 -0500

    added Jbuttons to loggedInView

--- src/view/LoggedInView.java
+++ src/view/LoggedInView.java
@@ -1,4 +1,66 @@
 package view;
 
-public class LoggedInView {
+import interface_adapter.logged_in.LoggedInViewModel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class LoggedInView extends JPanel implements ActionListener, PropertyChangeListener {
+
+    public final String viewName = "Logged In";
+    private final LoggedInViewModel loggedInViewModel;
+
+
+    JLabel username;
+    final JButton logOut;
+    final JButton inventory;
+    final JButton restriction;
+    final JButton getRecipe;
+
+    //a window with title and 4 JButtons
+    public LoggedInView(LoggedInViewModel loggedInViewModel) {
+        this.loggedInViewModel = loggedInViewModel;
+        this.loggedInViewModel.addPropertyChangeListener(this);
+
+        JLabel title = new JLabel("Logged In Screen");
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JLabel usernameInfo = new JLabel("Hello ");
+        username = new JLabel();
+
+        JPanel buttons = new JPanel();
+        inventory = new JButton(loggedInViewModel.INVENTORY_BUTTON_LABEL);
+        buttons.add(inventory);
+        restriction = new JButton(loggedInViewModel.RESTRICTION_BUTTON_LABEL);
+        buttons.add(restriction);
+        getRecipe = new JButton(loggedInViewModel.RECIPEGETTER_BUTTON_LABLE);
+        buttons.add(getRecipe);
+        logOut = new JButton(loggedInViewModel.LOGOUT_BUTTON_LABEL);
+        buttons.add(logOut);
+
+        inventory.addActionListener(
+                new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+
+                    }
+                }
+        );
+        restriction.addActionListener(this);
+        getRecipe.addActionListener(this);
+        logOut.addActionListener(this);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+
+    }
 }
