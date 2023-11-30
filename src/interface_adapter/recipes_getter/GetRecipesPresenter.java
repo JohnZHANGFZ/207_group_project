7a7dc1b765a8c6bbbc4dd95f0fe235f69ee2bdbe
Mike <wujimikechen@gmail.com>
2023-11-29 19:41:45 -0500

    made changes according to suggestions

--- src/interface_adapter/recipes_getter/GetRecipesPresenter.java
+++ src/interface_adapter/recipes_getter/GetRecipesPresenter.java
@@ -4,6 +4,8 @@ import interface_adapter.ViewManagerModel;
 import interface_adapter.add_item.AddItemViewModel;
 import interface_adapter.logged_in.LoggedInState;
 import interface_adapter.logged_in.LoggedInViewModel;
+import interface_adapter.recipe_result.ResultState;
+import interface_adapter.recipe_result.ResultViewModel;
 import use_case.recipes_getter.GetRecipeOutputBoundary;
 import use_case.recipes_getter.GetRecipeOutputData;
 
@@ -11,22 +13,23 @@ public class GetRecipesPresenter implements GetRecipeOutputBoundary {
 
     private final GetRecipesViewModel getRecipesViewModel;
 
-    private final LoggedInViewModel loggedInViewModel;
+    private final ResultViewModel resultViewModel;
 
     private ViewManagerModel viewManagerModel;
 
-    public GetRecipesPresenter(GetRecipesViewModel getRecipesViewModel, LoggedInViewModel loggedInViewModel) {
+    public GetRecipesPresenter(GetRecipesViewModel getRecipesViewModel, ResultViewModel resultViewModel, ViewManagerModel viewManagerModel) {
         this.getRecipesViewModel = getRecipesViewModel;
-        this.loggedInViewModel = loggedInViewModel;
+        this.resultViewModel = resultViewModel;
+        this.viewManagerModel = viewManagerModel;
     }
     @Override
     public void prepareSuccessView(GetRecipeOutputData results) {
-        GetRecipesState getRecipesState = getRecipesViewModel.getState();
-        getRecipesState.setRecipes(results.ReturnRecipe());
-        this.getRecipesViewModel.setState(getRecipesState);
-        this.getRecipesViewModel.firePropertyChanged();
+        ResultState resultViewModelState = resultViewModel.getState();
+        resultViewModelState.setRecipes(results.ReturnRecipe());
+        this.resultViewModel.setState(resultViewModelState);
+        this.resultViewModel.firePropertyChanged();
 
-        this.viewManagerModel.setActiveView(getRecipesViewModel.getViewName());
+        this.viewManagerModel.setActiveView(resultViewModel.getViewName());
         this.viewManagerModel.firePropertyChanged();
     }
 
