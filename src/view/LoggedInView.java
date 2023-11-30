844329b1f3b4e19e4a0ba53483137efe36893505
Alexa <alexahs.zhang@mail.utoronto.ca>
2023-11-29 19:32:37 -0500

    updated LoggedInView

--- src/view/LoggedInView.java
+++ src/view/LoggedInView.java
@@ -1,7 +1,7 @@
 package view;
 
-import interface_adapter.inventory.InventoryState;
 import interface_adapter.inventory.InventoryViewModel;
+import interface_adapter.logged_in.LoggedInState;
 import interface_adapter.logged_in.LoggedInViewModel;
 
 import javax.swing.*;
@@ -11,11 +11,12 @@ import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
+
+// TODO add inventory info to the view
 public class LoggedInView extends JPanel implements ActionListener, PropertyChangeListener {
 
     public final String viewName = "Logged In";
     private final LoggedInViewModel loggedInViewModel;
-    private final InventoryViewModel inventoryViewModel;
 
 
     JLabel username;
@@ -25,9 +26,8 @@ public class LoggedInView extends JPanel implements ActionListener, PropertyChan
     final JButton getRecipe;
 
     //a window with title and 4 JButtons
-    public LoggedInView(LoggedInViewModel loggedInViewModel, InventoryViewModel inventoryViewModel) {
+    public LoggedInView(LoggedInViewModel loggedInViewModel) {
         this.loggedInViewModel = loggedInViewModel;
-        this.inventoryViewModel = inventoryViewModel;
         this.loggedInViewModel.addPropertyChangeListener(this);
 
         JLabel title = new JLabel("Logged In Screen");
@@ -37,6 +37,7 @@ public class LoggedInView extends JPanel implements ActionListener, PropertyChan
         username = new JLabel();
 
         JPanel buttons = new JPanel();
+
         inventory = new JButton(loggedInViewModel.INVENTORY_BUTTON_LABEL);
         buttons.add(inventory);
         restriction = new JButton(loggedInViewModel.RESTRICTION_BUTTON_LABEL);
@@ -46,27 +47,30 @@ public class LoggedInView extends JPanel implements ActionListener, PropertyChan
         logOut = new JButton(loggedInViewModel.LOGOUT_BUTTON_LABEL);
         buttons.add(logOut);
 
-        inventory.addActionListener(
-                new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        if (e.getSource().equals(inventory)){
-                            InventoryState currentState = inventoryViewModel.getState();
-                        }
-                    }
-                }
-        );
+        // TODO Add the body to the actionPerformed method of the action listener below
+        //      for all buttons.
+        inventory.addActionListener(this);
         restriction.addActionListener(this);
         getRecipe.addActionListener(this);
         logOut.addActionListener(this);
+
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        this.add(title);
+        this.add(usernameInfo);
+        this.add(buttons);
+
     }
 
-    public void actionPerformed(ActionEvent e) {
+    //React to a button click that results in e.
 
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Click " + e.getActionCommand());
     }
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
-
+        LoggedInState state = (LoggedInState) evt.getNewValue();
+        username.setText(state.getUsername());
     }
 }
