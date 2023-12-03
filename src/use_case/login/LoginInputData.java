26b248eebbce79a26f0acfd241ea572bd9a7f566
JohnZHANGFZ <fangzheng.zhang@mail.utoronto.ca>
2023-12-03 18:13:32 -0500

    add some methods to test the validation of username and password

--- src/use_case/login/LoginInputData.java
+++ src/use_case/login/LoginInputData.java
@@ -16,4 +16,35 @@ public class LoginInputData {
     public String getPassword() {
         return password;
     }
+    public boolean isUsernameEmpty() {
+        return username.isEmpty();
+    }
+    public boolean isPasswordEmpty() {
+        return password.isEmpty();
+    }
+    public boolean isUsernameTooShort() {
+        return username.length() < 3;
+    }
+    public boolean isPasswordTooShort() {
+        return password.length() < 3;
+    }
+    public boolean isUsernameTooLong() {
+        return username.length() > 20;
+    }
+    public boolean isPasswordTooLong() {
+        return password.length() > 20;
+    }
+    public boolean isUsernameInvalid() {
+        return !username.matches("[a-zA-Z0-9]+");
+    }
+    public boolean isPasswordInvalid() {
+        return !password.matches("[a-zA-Z0-9]+");
+    }
+    public boolean isUsernameValid() {
+        return !isUsernameEmpty() && !isUsernameTooShort() && !isUsernameTooLong() && !isUsernameInvalid();
+    }
+    public boolean isPasswordValid() {
+        return !isPasswordEmpty() && !isPasswordTooShort() && !isPasswordTooLong() && !isPasswordInvalid();
+    }
+
 }
