b596fc7f800ae8dd78b2dc7074b922bdb650360e
JohnZHANGFZ <fangzheng.zhang@mail.utoronto.ca>
2023-12-03 17:11:21 -0500

    implement interactor

--- src/use_case/login/LoginInteractor.java
+++ src/use_case/login/LoginInteractor.java
@@ -1,5 +1,34 @@
 package use_case.login;
 
-public class LoginInteractor {
+import entity.User;
 
-}
+public class LoginInteractor implements LoginInputBoundary {
+     private final LoginUserDataAccessInterface loginUserDataAccessInterface;
+     private final LoginOutputBoundary loginOutputBoundary;
+
+     public LoginInteractor(LoginUserDataAccessInterface loginUserDataAccessInterface,
+                            LoginOutputBoundary loginOutputBoundary) {
+         this.loginUserDataAccessInterface = loginUserDataAccessInterface;
+         this.loginOutputBoundary = loginOutputBoundary;
+     }
+
+     @Override
+     public void execute(LoginInputData loginInputData) {
+         String username = loginInputData.getUsername();
+         String password = loginInputData.getPassword();
+         if (!loginUserDataAccessInterface.existsByName(username)) {
+             loginOutputBoundary.prepareFailView(username + ": Account does not exist.");
+         } else {
+             String pwd = loginUserDataAccessInterface.get(username).getPassword();
+             if (!password.equals(pwd)) {
+                 loginOutputBoundary.prepareFailView("Incorrect password for " + username + ".");
+             } else {
+
+                 User user = loginUserDataAccessInterface.get(loginInputData.getUsername());
+
+                 LoginOutputData loginOutputData = new LoginOutputData(user.getName(), false);
+                 loginPresenter.prepareSuccessView(loginOutputData);
+             }
+         }
+     }
+ }
