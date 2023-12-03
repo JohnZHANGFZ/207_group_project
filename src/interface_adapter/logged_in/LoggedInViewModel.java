00b2357ff43f541e1f04a29e1f597a05c4b9e62b
cher-ning <cher.ning@mail.utoronto.ca>
2023-12-03 18:24:30 -0500

    Removed LoggedIn controller and presenter, implemented viewmodel and state

--- src/interface_adapter/logged_in/LoggedInViewModel.java
+++ src/interface_adapter/logged_in/LoggedInViewModel.java
@@ -3,10 +3,18 @@ package interface_adapter.logged_in;
 import interface_adapter.ViewModel;
 
 import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 
 public class LoggedInViewModel extends ViewModel {
 
+    public final String TITLE_LABEL = "Logged In View";
+
     private LoggedInState state = new LoggedInState();
+
+    public static final String LOGOUT_BUTTON_LABEL = "Log out";
+
+    private String loggedInUser;
+
     public LoggedInViewModel(String viewName) {
         super(viewName);
     }
@@ -15,13 +23,23 @@ public class LoggedInViewModel extends ViewModel {
 
     public void setState(LoggedInState state) {this.state = state; }
 
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
     @Override
     public void firePropertyChanged() {
-
+        support.firePropertyChange("state", null, this.state);
     }
 
     @Override
     public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
 
+    public String getLoggedInUser() {
+        return loggedInUser;
+    }
+
+    public void setLoggedInUser(String loggedInUser) {
+        this.loggedInUser = loggedInUser;
     }
 }
