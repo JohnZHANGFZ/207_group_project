41c47161c8cf658fd36e79edee62dd387bb3af7c
cher-ning <cher.ning@mail.utoronto.ca>
2023-12-03 18:09:25 -0500

    completed LogoutInteractor

--- src/use_case/logout/LogoutInteractor.java
+++ src/use_case/logout/LogoutInteractor.java
@@ -1,8 +1,16 @@
 package use_case.logout;
 
+import entity.UserFactory;
+
 public class LogoutInteractor implements LogoutInputBoundary {
+
+    final LogoutOutputBoundary logoutPresenter;
+    public LogoutInteractor(LogoutOutputBoundary logoutOutputBoundary) {
+        this.logoutPresenter = logoutOutputBoundary;
+    }
+
     @Override
     public void execute() {
-
+        logoutPresenter.prepareSuccessView();
     }
 }
