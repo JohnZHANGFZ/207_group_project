377586fea58499fbdff3ca005ceb7b4294ecfe86
JohnZHANGFZ <fangzheng.zhang@mail.utoronto.ca>
2023-12-03 15:54:29 -0500

    update for input boundary and interactor

--- src/use_case/ReturnToPreviousView/ReturnInputBoundary.java
+++ src/use_case/ReturnToPreviousView/ReturnInputBoundary.java
@@ -1,4 +1,7 @@
 package use_case.ReturnToPreviousView;
 
+import use_case.signup.SignupInputData;
+
 public interface ReturnInputBoundary {
+    void navigateBack();
 }
--- src/use_case/ReturnToPreviousView/ReturnInteractor.java
+++ src/use_case/ReturnToPreviousView/ReturnInteractor.java
@@ -1,4 +1,19 @@
 package use_case.ReturnToPreviousView;
+import interface_adapter.ReturnToPreviousView.ReturnPresenter;
+public class ReturnInteractor implements ReturnInputBoundary{
+    private ReturnOutputBoundary ReturnPresenter;
+    private ViewStack viewStack;
 
-public class ReturnInteractor {
+    public ReturnInteractor(ReturnOutputBoundary ReturnPresenter, ViewStack viewStack) {
+        this.ReturnPresenter = ReturnPresenter;
+        this.viewStack = viewStack;
+    }
+
+    @Override
+    public void navigateBack() {
+        String previousView = viewStack.pop();
+        // check if the viewstack is empty and other necessary che
+
+        outputBoundary.updateView(previousView);
+    }
 }
