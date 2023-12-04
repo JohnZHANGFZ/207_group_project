2d2672fd6a5d6900b6a647e5946aa0d10419e370
Mike <wujimikechen@gmail.com>
2023-12-04 12:30:01 -0500

    added getid to RecipeInfoState

--- src/interface_adapter/recipe_information_getter/RecipeInfoState.java
+++ src/interface_adapter/recipe_information_getter/RecipeInfoState.java
@@ -6,15 +6,19 @@ import interface_adapter.recipe_info_result.ResultInfoState;
 public class RecipeInfoState {
     private String resultError = null;
     private JsonObject recipe;
+    private String id;
 
     public RecipeInfoState(RecipeInfoState copy) {
         resultError = copy.resultError;
         recipe = copy.recipe;
+        id = copy.id;
     }
 
     public RecipeInfoState() {
 
     }
+
+    public String getID(){return id; }
 
     public JsonObject getRecipe() {
         return recipe;
