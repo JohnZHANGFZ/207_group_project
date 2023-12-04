99db6d980cf273e184eff6f017e3807e5c6a9e68
cher-ning <cher.ning@mail.utoronto.ca>
2023-12-04 17:24:11 -0500

    Added get/set methods for ingredients and numRecipes input boxes

--- src/interface_adapter/logged_in/LoggedInState.java
+++ src/interface_adapter/logged_in/LoggedInState.java
@@ -2,6 +2,8 @@ package interface_adapter.logged_in;
 
 public class LoggedInState {
     private String username = "";
+    private String ingredients = "";
+    private int numRecipes;
 
     public LoggedInState(LoggedInState copy) {
         username = copy.username;
@@ -17,4 +19,12 @@ public class LoggedInState {
     public void setUsername(String username) {
         this.username = username;
     }
+
+    public String getIngredients() {return ingredients;}
+
+    public void setIngredients(String ingredients) {this.ingredients = ingredients;}
+
+    public int getNumRecipes() {return numRecipes;}
+
+    public void setNumRecipes(int numRecipes) {this.numRecipes = numRecipes;}
 }
