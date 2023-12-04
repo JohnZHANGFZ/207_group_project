a1f75f93b4f315fd725e283774cb2c474bb2e429
cher-ning <cher.ning@mail.utoronto.ca>
2023-12-04 11:33:59 -0500

    Implemented basic loggedInView with log out button, still need to add Inventory+Restriction info and buttons

--- src/view/LoggedInView.java
+++ src/view/LoggedInView.java
@@ -1,4 +1,54 @@
 package view;
 
-public class LoggedInView {
+import interface_adapter.logged_in.LoggedInState;
+import interface_adapter.logged_in.LoggedInViewModel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class LoggedInView extends JPanel implements ActionListener, PropertyChangeListener {
+
+    public final String viewName = "logged in";
+    private final LoggedInViewModel loggedInViewModel;
+
+    JLabel username;
+
+    final JButton logOut;
+
+    public LoggedInView(LoggedInViewModel loggedInViewModel) {
+        this.loggedInViewModel = loggedInViewModel;
+        this.loggedInViewModel.addPropertyChangeListener(this);
+
+        JLabel title = new JLabel("Logged In");
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JLabel welcomeBack = new JLabel("Welcome back,");
+        username = new JLabel();
+
+        JPanel buttons = new JPanel();
+        logOut = new JButton(loggedInViewModel.LOGOUT_BUTTON_LABEL);
+        buttons.add(logOut);
+        logOut.addActionListener(this);
+
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.add(title);
+        this.add(welcomeBack);
+        this.add(username);
+        this.add(buttons);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent evt) {
+        System.out.println("Click " + evt.getActionCommand());
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        LoggedInState state = (LoggedInState) evt.getNewValue();
+        username.setText(state.getUsername());
+    }
 }
