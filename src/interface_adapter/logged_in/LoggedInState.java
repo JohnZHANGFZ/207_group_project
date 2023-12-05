ca672e19bbbf81db23f9929885a5eed9054a6749
cher-ning <cher.ning@mail.utoronto.ca>
2023-12-05 14:53:45 -0500

    Added userRestriction and userRestriction info to loginOutPut and appropriate getters/setters

--- src/interface_adapter/logged_in/LoggedInState.java
+++ src/interface_adapter/logged_in/LoggedInState.java
@@ -2,6 +2,8 @@ package interface_adapter.logged_in;
 
 public class LoggedInState {
     private String username = "";
+    private String userInventory = "";
+    private String userRestrictions = "";
     private String ingredients = "";
     // leaving numRecipes stored in state as a String so that textbox can be updated regardless of what is typed
     private String numRecipes = "";
@@ -21,11 +23,19 @@ public class LoggedInState {
         this.username = username;
     }
 
-    public String getIngredients() {return ingredients;}
+    public String getUserInventory() {return userInventory;} // for user's stored inventory
 
-    public void setIngredients(String ingredients) {this.ingredients = ingredients;}
+    public void setUserInventory(String userInventory) {this.userInventory = userInventory;} // for user's stored inventory
 
-    public String getNumRecipes() {return numRecipes;}
+    public String getUserRestrictions() {return userRestrictions;} // for user's stored allergies
 
-    public void setNumRecipes(String numRecipes) {this.numRecipes = numRecipes;}
+    public void setUserRestrictions(String userRestrictions) {this.userRestrictions = userRestrictions;} // for user's stored allergies
+
+    public String getIngredients() {return ingredients;} // for ingredient text box input
+
+    public void setIngredients(String ingredients) {this.ingredients = ingredients;} // for ingredient text box input
+
+    public String getNumRecipes() {return numRecipes;} // for numRecipe textbox input
+
+    public void setNumRecipes(String numRecipes) {this.numRecipes = numRecipes;} // for numRecipe textbox input
 }
