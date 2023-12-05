f10570ba3992aa79dfb70816302207345cc0e88f
JohnZHANGFZ <fangzheng.zhang@mail.utoronto.ca>
2023-12-05 01:12:29 -0500

    implement state

--- src/interface_adapter/ReturnToPreviousView/ReturnState.java
+++ src/interface_adapter/ReturnToPreviousView/ReturnState.java
@@ -1,2 +1,31 @@
-package interface_adapter.ReturnToPreviousView;public class ReturnState {
+package interface_adapter.ReturnToPreviousView;
+
+import java.util.Stack;
+
+public class ReturnState {
+    private Stack<String> viewHistory = new Stack<>();
+
+    public void addView(String viewName) {
+        if (viewHistory.isEmpty() || !viewHistory.peek().equals(viewName)) {
+            viewHistory.push(viewName);
+        }
+    }
+
+    public boolean canGoBack() {
+        return viewHistory.size() > 1;
+    }
+
+    public String getPreviousView() {
+        if (canGoBack()) {
+            viewHistory.pop(); // current view
+            return viewHistory.peek(); // last view
+        }
+        return null;
+    }
+
+    public void goBack() {
+        if (canGoBack()) {
+            viewHistory.pop(); // remove current view
+        }
+    }
 }
