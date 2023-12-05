5615db48b6e0e1b784542c711c28209294caeba5
JohnZHANGFZ <fangzheng.zhang@mail.utoronto.ca>
2023-12-05 15:18:07 -0500

    make it be obervable

--- src/use_case/ReturnToPreviousView/ReturnInteractor.java
+++ src/use_case/ReturnToPreviousView/ReturnInteractor.java
@@ -1,13 +1,25 @@
 package use_case.ReturnToPreviousView;
+import java.util.ArrayList;
+import java.util.List;
 import interface_adapter.ReturnToPreviousView.ReturnState;
+import interface_adapter.ReturnToPreviousView.ViewChangeObserver;
 
-public class ReturnInteractor implements ReturnInputBoundary{
+public class ReturnInteractor implements ReturnInputBoundary {
     private ReturnState returnState;
-
+    private List<ViewChangeObserver> observers = new ArrayList<>();
 
     public ReturnInteractor(ReturnState returnState) {
         this.returnState = returnState;
+    }
 
+    public void addObserver(ViewChangeObserver observer) {
+        observers.add(observer);
+    }
+
+    private void notifyViewChange(String viewName) {
+        for (ViewChangeObserver observer : observers) {
+            observer.onViewChange(viewName);
+        }
     }
 
     @Override
@@ -15,7 +27,6 @@ public class ReturnInteractor implements ReturnInputBoundary{
         if (returnState.canGoBack()) {
             String previousView = returnState.getPreviousView();
             returnState.goBack();
-            // TODO: need a mechanism to notify the view layer that it needs to switch views
             notifyViewChange(previousView);
         }
     }
