dedf546884c6adefbd1f7dfe4285c06dc5eae142
JohnZHANGFZ <fangzheng.zhang@mail.utoronto.ca>
2023-12-05 01:11:02 -0500

    implement interactor

---
+++ src/interface_adapter/ReturnToPreviousView/ReturnState.java
@@ -0,0 +1,2 @@
+package interface_adapter.ReturnToPreviousView;public class ReturnState {
+}
--- src/use_case/ReturnToPreviousView/ReturnInteractor.java
+++ src/use_case/ReturnToPreviousView/ReturnInteractor.java
@@ -1,20 +1,22 @@
 package use_case.ReturnToPreviousView;
-import interface_adapter.ReturnToPreviousView.ReturnPresenter;
+import interface_adapter.ReturnToPreviousView.ReturnState;
 
 public class ReturnInteractor implements ReturnInputBoundary{
-    private ReturnOutputBoundary ReturnPresenter;
-    private ViewStack viewStack;
+    private ReturnState returnState;
 
-    public ReturnInteractor(ReturnOutputBoundary ReturnPresenter, ViewStack viewStack) {
-        this.ReturnPresenter = ReturnPresenter;
-        this.viewStack = viewStack;
+
+    public ReturnInteractor(ReturnState returnState) {
+        this.returnState = returnState;
+
     }
 
     @Override
     public void navigateBack() {
-        String previousView = viewStack.pop();
-        // check if the viewstack is empty and other necessary che
-
-        outputBoundary.updateView(previousView);
+        if (returnState.canGoBack()) {
+            String previousView = returnState.getPreviousView();
+            returnState.goBack();
+            // TODO: need a mechanism to notify the view layer that it needs to switch views
+            notifyViewChange(previousView);
+        }
     }
 }
