9a552018d9fd2e9fe7bc8fe08ea04d3e3c7608a1
JohnZHANGFZ <fangzheng.zhang@mail.utoronto.ca>
2023-12-06 16:13:00 -0500

    temporary upodate to switch branch

--- src/view/DetailsView.java
+++ src/view/DetailsView.java
@@ -1,5 +1,7 @@
 package view;
 
+import interface_adapter.recipe_info_result.ResultInfoViewModel;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,6 +11,41 @@ import java.beans.PropertyChangeListener;
 public class DetailsView extends JPanel implements ActionListener, PropertyChangeListener {
 
     public final String viewName = "Details";
+    public DetailsView(ResultInfoViewModel resultInfoViewModel) {
+
+        JLabel title = new JLabel("Recipe Details Screen");
+        title.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+
+        // JLabel recipeName = new JLabel("Recipe Name: " + recipeInfoViewModel.getState().getName());
+        // JLabel recipeIngredients = new JLabel("Recipe Ingredients: " + recipeInfoViewModel.getState().getIngredients());
+        JLabel recipeInstructions = new JLabel("Recipe Instructions: " + recipeInfoViewModel.getState().getInstructions());
+
+
+        //add buttons
+        JPanel buttons = new JPanel();
+
+        JButton cancel = new JButton("Cancel");
+        buttons.add(cancel);
+
+        cancel.addActionListener(
+                new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        recipeInfoViewModel.getState().setID(id);
+
+                    }
+
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+
+                    }
+
+                    @Override
+                    public void propertyChange(PropertyChangeEvent evt) {
+
+                    }
+                }
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
