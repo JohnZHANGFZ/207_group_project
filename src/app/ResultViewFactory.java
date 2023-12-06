00ff295f922a49ee0df20be3f3ca8c25b70ada59
JohnZHANGFZ <fangzheng.zhang@mail.utoronto.ca>
2023-12-05 22:14:14 -0500

    implement resultviewfactory.java to maintain the simplify of main.java

--- src/app/ResultViewFactory.java
+++ src/app/ResultViewFactory.java
@@ -1,2 +1,46 @@
-package app;public class ResultViewFactory {
+package app;
+
+import interface_adapter.ViewManagerModel;
+import interface_adapter.recipe_info_result.ResultInfoViewModel;
+import interface_adapter.recipe_information_getter.RecipeInfoController;
+import interface_adapter.recipe_information_getter.RecipeInfoPresenter;
+import interface_adapter.recipe_information_getter.RecipeInfoViewModel;
+import interface_adapter.recipes_getter.GetRecipesViewModel;
+import search_recipe.QueryAPI;
+import use_case.recipe_information_getter.RecipeInfoDataAccessInterface;
+import use_case.recipe_information_getter.RecipeInfoInputBoundary;
+import use_case.recipe_information_getter.RecipeInfoInteractor;
+import use_case.recipe_information_getter.RecipeInfoOutputBoundary;
+import view.ResultView;
+
+import javax.swing.*;
+import java.io.IOException;
+
+public class ResultViewFactory {
+    private ResultViewFactory() {}
+
+    public static ResultView create(GetRecipesViewModel getRecipesViewModel,
+                                    RecipeInfoViewModel recipeInfoViewModel,
+                                    ResultInfoViewModel resultInfoViewModel,
+                                    ViewManagerModel viewManagerModel) {
+        try {
+            RecipeInfoController recipeInfoController = createRecipeInfoUseCase(recipeInfoViewModel,
+                    resultInfoViewModel, viewManagerModel);
+            return new ResultView(getRecipesViewModel, recipeInfoViewModel, recipeInfoController);
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Could not open APIKey file.");
+        }
+        return null;
+    }
+
+    private static RecipeInfoController createRecipeInfoUseCase(RecipeInfoViewModel recipeInfoViewModel,
+                                                                ResultInfoViewModel resultInfoViewModel,
+                                                                ViewManagerModel viewManagerModel) throws IOException {
+        RecipeInfoOutputBoundary recipeInfoPresenter = new RecipeInfoPresenter(recipeInfoViewModel,
+                resultInfoViewModel, viewManagerModel);
+        RecipeInfoDataAccessInterface recipeInfoDataAccessInterface = new QueryAPI();
+        RecipeInfoInputBoundary recipeInfoInteractor = new RecipeInfoInteractor(recipeInfoDataAccessInterface,
+                recipeInfoPresenter);
+        return new RecipeInfoController(recipeInfoInteractor);
+    }
 }
