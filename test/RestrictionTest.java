cbc38dc2185e4f4b1d1c3d8e47610ba6f438c8ae
Mike <wujimikechen@gmail.com>
2023-12-06 23:10:24 -0500

    adding tests

--- test/RestrictionTest.java
+++ test/RestrictionTest.java
@@ -1,18 +1,39 @@
+import entity.IngredientFactory;
 import interface_adapter.ViewManagerModel;
 import interface_adapter.restriction.RestrictionController;
 import interface_adapter.restriction.RestrictionPresenter;
 import interface_adapter.restriction.RestrictionState;
 import interface_adapter.restriction.RestrictionViewModel;
 import org.junit.Test;
-import use_case.edit_inventory.EditInventoryInputBoundary;
+import use_case.add_dietary_restriction.*;
 import use_case.edit_restriction.EditRestrictionInputBoundary;
+import use_case.recipes_getter.GetRecipeOutputBoundary;
 
 import static org.mockito.Mockito.*;
-import java.awt.*;
+
 import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 
 public class RestrictionTest {
+
+    @Test
+    public void useCaseTest() {
+        AddRestrictionInputData addRestrictionInputData = new AddRestrictionInputData(new ArrayList<>());
+        addRestrictionInputData.getItems();
+
+        AddRestrictionOutputData addRestrictionOutputData = new AddRestrictionOutputData(new ArrayList<>(), false);
+        addRestrictionOutputData.GetIngredient();
+
+        AddRestrictionUserDataAccessInterface addRestrictionUserDataAccessInterface = mock(AddRestrictionUserDataAccessInterface.class);
+
+        AddRestrictionOutputBoundary addRestrictionOutputBoundary = mock(AddRestrictionOutputBoundary.class);
+
+        IngredientFactory ingredientFactory = mock(IngredientFactory.class);
+
+        AddRestrictionInteractor addRestrictionInteractor = new AddRestrictionInteractor(addRestrictionUserDataAccessInterface, addRestrictionOutputBoundary, ingredientFactory);
+
+        addRestrictionInteractor.execute(addRestrictionInputData);
+    }
     @Test
     public void presenterTest() {
         RestrictionState restrictionState = new RestrictionState();
