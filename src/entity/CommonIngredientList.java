06f2e51695536479fabe74c01cf63ffb9b20a421
Mike <wujimikechen@gmail.com>
2023-12-07 06:11:09 -0500

    adding tests

---
+++ src/entity/CommonIngredientList.java
@@ -0,0 +1,15 @@
+package entity;
+
+import java.util.ArrayList;
+
+public class CommonIngredientList {
+    private ArrayList<CommonIngredient> ingredients;
+
+    public void ingredientsListAdd(CommonIngredient s) {
+        this.ingredients.add(s);
+    }
+
+    public void addAll(ArrayList<CommonIngredient> s) {
+        ingredients.addAll(s);
+    }
+}
